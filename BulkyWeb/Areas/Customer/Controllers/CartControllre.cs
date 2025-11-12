0c588a79f1f54c8c2724db8caff765af82b0f0b1
sawc21 <Sawyer.Cawthon@gmail.com>
2025-11-12 11:56:42 -0600

    New changes

---
+++ BulkyWeb/Areas/Customer/Controllers/CartControllre.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    public class CartControllre : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
